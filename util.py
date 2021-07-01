f5eff8201eade0907355bb93d68bf46cb2c75b01
JingYun Wang <jingyunwang24@gmail.com>
2021-07-01 16:51:21 -0400

    Organize functions

--- util.py
+++ util.py
@@ -1,11 +1,29 @@
-# Utility file for Time Series Analysis
+# Utility file for Time Series Analysis #
 
+
+## Fetch Data from Yahoo Finance ##
+# 1 - select ticker
+# 2 - get stock data
+# 3 - get data (put multiple data in a single dataframe)
+
+
+## ARIMA ##
 # 1 - visualize time series
 # 2 - stationary check
 # 3 - decompostion
 # 4 - detrend method
 # 5 - ACF & PACF
-# 6 - evaluate
+
+
+## Sklearn Evaluation ##
+
+
+
+
+
+###############
+#   Imports   #
+###############
 
 # General
 import pandas as pd
@@ -13,7 +31,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # sklearn
-from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# Yahoo Finance
+import yfinance as yf
 
 # pmdarima
 import pmdarima as pm
@@ -32,6 +53,93 @@ from statsmodels.tsa.arima_model import ARIMA
 
 
 
+#####################################
+#   Fetch Data from Yahoo Finance   #
+#####################################
+
+# 1
+def select_ticker(symbols, year):
+    '''
+    Select ticker only fulfilled the requirement
+    year:
+    ex. 2011 => that stock has at least 2021-2011 = 10 years of hisory
+    '''
+    
+    timestamps = []
+    incep_year = []
+    
+    for symbol in symbols:
+        # Acquire the inception date for each ticker
+        timestamps.append(yf.Ticker(symbol).info.get('fundInceptionDate'))
+    
+    for timestamp in timestamps: 
+        # Convert epoch unix to readable dates
+        incep_year.append(pd.to_datetime(timestamp, unit='s').year)
+    
+    # Create a dictionary to pair the ticker with corresponding year
+    ticker_dict = dict(zip(symbols, incep_year))
+    
+    # Create a ticker df
+    ticker_df = pd.DataFrame(list(ticker_dict.items()), columns=['symbol', 'start_year'])
+    
+    # Select desire ticker based on inception date
+    new_ticker_df = ticker_df.loc[ticker_df['start_year'] <= year]
+    
+    new_ticker_list = new_ticker_df['symbol'].tolist()
+                                                     
+    return new_ticker_list
+                                                     
+                                                                                                         
+# 2                                                     
+def get_stock_data(symbols):
+    '''
+    Acquire the historical data from Yahoo Finance
+    for provided stock symbol
+    '''
+    
+    # Access ticker data
+    ticker = yf.Ticker(symbols)
+    
+    # Get historical market data
+    data = ticker.history(period='max')
+    
+    return data
+
+
+# 3
+def get_data(symbols):
+    '''
+    Acquire the data by calling previous function
+    Concatenate the dataframe
+    '''
+    
+    # Create a blank dataframe
+    df = pd.DataFrame()
+    
+    for symbol in symbols:
+        try:
+            # Get all historical market data for all tickers
+            df_extra = get_stock_data(symbol)
+            
+            # Add an extra column to label the ticker
+            df_extra['Ticker'] = symbol
+            
+            # Concatenate all the piece
+            df = pd.concat([df,df_extra])
+        
+        except:
+            print(f'Ticker error:{symbol}')
+     
+    return df
+
+
+
+
+
+#############
+#   ARIMA   #
+#############
+
 # 1
 def visualize_time_series(TS):
     '''
@@ -43,8 +151,7 @@ def visualize_time_series(TS):
     plt.show()
 
 
-
-# 2
+# 2a
 def stationary_check_statsmodels(TS, window_size):
     '''
     statsmodels
@@ -89,7 +196,7 @@ def stationary_check_statsmodels(TS, window_size):
     print('Results of Dickey-Fuller test: \n')
     print(dfoutput)
   
-    
+# 2b    
 def stationary_check_pmdarima(TS):
     '''
     pmdarima
@@ -102,9 +209,8 @@ def stationary_check_pmdarima(TS):
 
     print(f"P-Value: {p_val}, so should you difference the data? {should_diff}")
 
-
     
-# 3
+# 3a
 def decomposition_plot_pmdarima(TS, frequency):
     '''
     pmdarima
@@ -119,7 +225,7 @@ def decomposition_plot_pmdarima(TS, frequency):
     # Plot the decomposition plot
     decomposed_plot(decomposed, figure_kwargs={'figsize': (12,10)})
     
- 
+# 3b 
 def decomposition_plot_statsmodels(TS, frequency):
     '''
     statsmodels
@@ -157,8 +263,7 @@ def decomposition_plot_statsmodels(TS, frequency):
     plt.tight_layout()
 
 
-
-# 4
+# 4a
 def detrend_transformation(TS, log=False, sqrt=False):
     '''
     Log Transformation
@@ -179,7 +284,7 @@ def detrend_transformation(TS, log=False, sqrt=False):
         print("Error")
         return None
         
-
+# 4b
 def detrend_rolling_mean(TS, regular=False, window_size=None, half_life=None):
     '''
     Subtract rolling mean
@@ -202,7 +307,7 @@ def detrend_rolling_mean(TS, regular=False, window_size=None, half_life=None):
         TS_minus_exp_rolling_mean.dropna(inplace=True)
         return TS_minus_exp_rolling_mean
     
-
+# 4c
 def detrend_differencing(TS, periods):
     '''
     Just differencing method
@@ -214,23 +319,29 @@ def detrend_differencing(TS, periods):
     return TS_diff
 
 
-
-# 5
+# 5a
 def plot_ACF_PACF(TS):
     fig, (ax1, ax2) = plt.subplots(1,2, figsize=(18,4))
     plot_acf(TS, ax=ax1, lags=25)
     plot_pacf(TS, ax=ax2, lags=25)
 
+# 5b
 def pd_ACF(TS):
     pd.plotting.autocorrelation_plot(TS)
 
 
 
-# 6
+
+
+#########################
+#   Sklearn Evalution   #
+#########################
+
 def evaluate(y_true, y_pred):
-    MSE = mean_squared_error(y_true, y_pred)
     MAE = mean_absolute_error(y_true, y_pred)
-    MAPE = mean_absolute_percentage_error(y_true, y_pred)
-    print("MSE: %.3f " % MSE)
-    print("MAE: %.3f" % MAE)
-    print("MAPE:%.3f " % MAPE)
+    RMSE = mean_squared_error(y_true, y_pred, squared=False)
+    R2 = r2_score(y_true, y_pred)
+    
+    print("MAE: %.4f" % MAE)
+    print("RMSE: %.4f" % RMSE)
+    print("R^2: %.4f" % R2)
